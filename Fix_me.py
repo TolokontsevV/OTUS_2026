3842f770c862312da398d92539e4e3859dc4e01f
TolokontsevV <megarazum100@mail.ru>
2026-09-13 13:42:42 +0300

    feat: add flake8 version 2

--- Fix_me.py
+++ Fix_me.py
@@ -1,10 +1,10 @@
 def calculate_average(nums):
     total = sum(nums)
-    count = len(nums)
+    count = len(nums)  # правка numbers на nums
     average = total / count
-    return average
+    return average  # добавлена строка return. Добавлена двойная пустая строка
 
 
 nums = [10, 15, 20]
-Result = calculate_average(nums)
-print("The average is:", Result)
+result = calculate_average(nums)
+print("The average is:", result)  # правка Print на print, пустая строка
